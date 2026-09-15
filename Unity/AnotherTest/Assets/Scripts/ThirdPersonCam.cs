9c4613dedceb27f8f70cdd5b86c1f389a9221edd
DjDMaster <damienjduenas@gmail.com>
2026-09-14 23:19:27 -0600

    Scripts and Cleanup
    Movement, Camera, Jumping, Drag, 3rd Person Camera start and 1st Person Camera

--- Unity/AnotherTest/Assets/Scripts/ThirdPersonCam.cs
+++ Unity/AnotherTest/Assets/Scripts/ThirdPersonCam.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ThirdPersonCam : MonoBehaviour
@@ -10,19 +12,26 @@ public class ThirdPersonCam : MonoBehaviour
 
     public float rotationSpeed;
 
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+
     private void Update()
     {
         // rotate orientation
         Vector3 viewDir = Player.position  - new Vector3(transform.position.x, Player.position.y, transform.position.z);
-        Orientation.foward = viewDir.normalized;
+        Orientation.forward = viewDir.normalized;
 
         // rotate player object
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-        Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        Vector3 inputDir = Orientation.forward * verticalInput + Orientation.right * horizontalInput;
 
         if(inputDir != Vector3.zero)
-            PlayerObject.forward = Vector3.Slerp(PlayerObject.foward)
+            PlayerObject.forward = Vector3.Slerp(PlayerObject.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
 
     }
 }
